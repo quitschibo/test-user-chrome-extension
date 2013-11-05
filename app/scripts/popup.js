0d38f9861fd5baacc3416f42d51db819808bf966
Manuel Möhlmann <mail@manmoe.com>
2013-11-05 15:18:52 +0100

    added first functional version

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,52 @@
 'use strict';
+
+// function for generating a test user by http://randomuser.me/
+function getNewUser() {
+    $.ajax({dataType:'json', url: 'http://api.randomuser.me/', timeout:5000, success:onRssSuccess, error:onRssError, async: false});
+}
+
+// handle successful requests
+function onRssSuccess(results) {
+    var user = results.results[0].user;
+    $('#firstname').val(user.name.first);
+    $('#lastname').val(user.name.last);
+
+    $('#face').remove();
+    $('#facespace').prepend('<img id="face" src="' + user.picture + '" />');
+
+    console.log(user.picture);
+}
+
+// handle unsuccessful requests
+function onRssError(results) {
+    console.log('error');
+}
+
+
+function getEmail() {
+    $.ajax({dataType:'json', url: 'https://www.guerrillamail.com/ajax.php?f=get_email_address', timeout:5000, success:onEmailRssSuccess, error:onEmailRssError, async: false});
+}
+
+function onEmailRssSuccess(result) {
+    console.log(result.email_addr);
+    $('#email').val(result.email_addr);
+}
+
+function onEmailRssError(result) {
+    console.log('email cannot be retrieved due to an error');
+}
+
+/**
+ * Just do all calls, when button is clicked
+ */
+function doCalls() {
+    getNewUser();
+    getEmail();
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', doCalls);
+});
